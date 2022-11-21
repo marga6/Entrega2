f4d3176f24cf60165efa6c58ceef761f28bd8adb
marga6 <mpf_298@hotmail.com>
2022-11-21 09:30:07 +0100

    Añadiendo mi suma.py

---
+++ suma.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+from random import randrange
+
+x = (randrange(10,100))
+y = (randrange(10,100))
+z = (randrange(10,100))
+
+print ("suma = (x + y) + z")
+print(x+y+z,"= (",x,"+",y,")+",z,)
+
